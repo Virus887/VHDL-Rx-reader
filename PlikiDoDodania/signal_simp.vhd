d0155f00b284bbdc41e4fc0e30866b1f587944a5
leoniklexa@gmail.com <leoniklexa@gmail.com>
2021-04-28 21:49:26 +0200

    Complete copy paste professional godocoding

--- PlikiDoDodania/signal_simp.vhd
+++ PlikiDoDodania/signal_simp.vhd
@@ -54,119 +54,119 @@ begin
     buf_counter <= 0;
     buf <= (others => '0');
     error <= '0';
+    
+    
+   elsif rising_edge(C) then 
    
+    case state is
+        when CZEKANIE =>					
+           time_counter <= 0;					
+           buf_counter <= 0;					
+           buf   <= (others => '0');				
+           error <= '0';					
+           if (input(1)='0' and input(0)='1') then	
+             state   <= START;					
+           end if;					
+        when START =>
+            if (time_counter /= T/2) then
+                time_counter <= time_counter +1;
+            else
+                time_counter <= 0;
+                state <= DANA;
+                if(input(1) = '0') then
+                    error <= '1';
+                end if;
+            end  if;
+    
+        when DANA =>
+            if(time_counter /= T) then
+                time_counter <= time_counter + 1;
+            else
+                buf(buf'left) <= input(1);
+                buf(buf'left -1 downto 0) <= buf(buf'left downto 0);
+                time_counter <= 0;
+                
+                if(buf_counter /= B_SLOWA-1) then
+                    buf_counter <= buf_counter + 1;
+                else
+                    buf_counter <= 0;
+                    if(B_PARZYSTOSCI = 1) then
+                        state <= PARZYSTOSC;
+                    else
+                        state <= STOP;
+                    end if;
+                end if;
+            end if;
+    
+        when PARZYSTOSC =>
+           if (time_counter /= T) then				
+             time_counter <= time_counter + 1;		
+           else							
+                 time_counter <= 0;					
+             state <= STOP;				
+             if ((input(1) = XOR_REDUCE(buf)) = N_SLOWO) then
+                   error <= '1';					
+             end if; 					
+           end if;					
+        when DANA =>						
+           if (time_counter /= T) then			
+             time_counter <= time_counter + 1;			
+           else							
+             buf(buf'left) <= input(1);			
+             buf(buf'left-1 downto 0) <= buf(buf'left downto 1);
+                 time_counter <= 0;					
+             
+             if (buf_counter /= B_SLOWA-1) then			
+               buf_counter <= buf_counter + 1;		
+             else						
+               buf_counter <= 0;			
+               if (B_PARZYSTOSCI = 1) then		
+                 state <= PARZYSTOSC;		
+               else						
+                 state <= STOP;				
+               end if;						
+             end if; 					
+    
+           end if;						
+        when PARZYSTOSC =>				
+            if (time_counter /= T) then				
+             time_counter <= time_counter + 1;				
+            else					
+             time_counter <= 0;				
+             state <= STOP;				
+             if ((input(1) = XOR_REDUCE(buf)) = N_SLOWO) then
+                   error <= '1';					
+             end if; 					
+            end if;					
+	   
+        when STOP =>						
+            if (time_counter /= T) then				
+             time_counter <= time_counter + 1;			
+            else							
+                 time_counter <= 0;					
+            
+             if (buf_counter /= B_STOPOW-1) then			
+               buf_counter <= buf_counter + 1;				
+               if (input(1) /= '0') then		
+                     error <= '1';			
+               end if; 					
+             else					
+               if (error = '0' and input(1) = '0') then	
+                     SLOWO <= buf;				
+                     if (N_SLOWO = TRUE) then			
+                       SLOWO <= not(buf);				
+                     end if;				
+                     GOTOWE <= '1';					
+               else					
+                     SLOWO <= (others => '0');		
+                     BLAD <= '1';			
+               end if;					
+               state <= CZEKANIE;			
+             end if;					
+            end if;				
+    end case;
    end if;
-   
-   if rising_edge(C) then 
-   
-   end if;
-   
---     if (R='1') then						-- asynchroniczna inicjalizacja rejestrow
---       wejscie	<= (others => '0');				-- wyzerowanie rejestru sygnalu RX
---       stan	<= CZEKANIE;					-- poczatkowy stan pracy odbiornika
---       l_czasu  <= 0;						-- wyzerowanie licznika czasu bodu
---       l_bitow  <= 0;						-- wyzerowanie licznika odebranych bitow
---       bufor	<= (others => '0');				-- wyzerowanie bufora bitow danych
---       problem 	<= '0';						-- wyzerowanie rejestru bledu odbioru			
---       SLOWO	<= (others => '0');				-- wyzerowanie wyjsciowego slowa danych
---       GOTOWE	<= '0';						-- wyzerowanie flagi potwierdzenia odbioru
---       BLAD	<= '0';						-- wyzerowanie flagi wykrycia bledu w odbiorze
 
---     elsif (rising_edge(C)) then				-- synchroniczna praca odbiornika
-
---       GOTOWE     <= '0';					-- defaultowe skasowanie flagi potwierdzenia odbioru
---       BLAD       <= '0';					-- defaultowe skasowanie flagi wykrycia bledu w odbiorze
---       wejscie(0) <= RX;					-- zarejestrowanie synchroniczne stanu sygnalu RX
---       if (N_RX = TRUE) then					-- badanie warunku zanegowania sygnalu szeregowego
---         wejscie(0) <= not(RX);					-- zarejestrowanie synchroniczne zanegowanego sygnalu RX
---       end if;							-- zakonczenie instukcji warunkowej  
---       wejscie(1) <= wejscie(0);				-- zarejestrowanie dwoch kolejnych stanow sygnalu RX
-
---       case stan is						-- badanie aktualnego stanu maszyny stanow 
-
---         when CZEKANIE =>					-- obsluga stanu CZEKANIE
---           l_czasu <= 0;					-- wyzerowanie licznika czasu bodu
---           l_bitow <= 0;					-- wyzerowanie licznika odebranych bitow
---           bufor   <= (others => '0');				-- wyzerowanie bufora bitow danych
---           problem <= '0';					-- wyzerowanie rejestru bledu odbioru
---	   if (wejscie(1)='0' and wejscie(0)='1') then		-- wykrycie poczatku bitu START
---	     stan   <= START;					-- przejscie do stanu START
---	   end if;						-- zakonczenie instukcji warunkowej
-
---         when START =>						-- obsluga stanu START
---	   if (l_czasu /= T/2) then				-- badanie odliczania okresu T/2
---	     l_czasu <= l_czasu + 1;				-- zwiekszenie o 1 stanu licznika czasu
---	   else							-- zakonczenie odliczania czasu T/2
---             l_czasu <= 0;					-- wyzerowanie licznika czasu bodu
---	     stan    <= DANA;					-- przejscie do stanu DANA
---	     if (wejscie(1) = '0') then				-- badanie nieprawidlowego stanu bitu START
---               problem <= '1';					-- ustawienie rejestru bledu odbioru
---	     end if;						-- zakonczenie instukcji warunkowej  
---	   end if;						-- zakonczenie instukcji warunkowej
-
---         when DANA =>						-- obsluga stanu DANA
---	   if (l_czasu /= T) then				-- badanie odliczania okresu T
---	     l_czasu <= l_czasu + 1;				-- zwiekszenie o 1 stanu licznika czasu
---	   else							-- zakonczenie odliczania czasu T
---	     bufor(bufor'left) <= wejscie(1);			-- zapamietanie stanu bitu danych
---	     bufor(bufor'left-1 downto 0) <= bufor(bufor'left downto 1); -- przesuniecie bitow w buforze
---             l_czasu <= 0;					-- wyzerowanie licznika czasu bodu
-	     
---	     if (l_bitow /= B_SLOWA-1) then			-- badanie odliczania bitow danych
---	       l_bitow <= l_bitow + 1;				-- zwiekszenie o 1 liczby bitow danych
---	     else						-- zakonczenie odliczania bitow danych
---	       l_bitow <= 0;					-- wyzerowanie licznika odebranych bitow
---	       if (B_PARZYSTOSCI = 1) then			-- badanie odbioru bitu parzystosci
---	         stan <= PARZYSTOSC;				-- przejscie do stanu PARZYSTOSC
---	       else						-- brak odbioru bitu parzystosci  
---	         stan <= STOP;					-- przejscie do stanu STOP
---	       end if;						-- zakonczenie instukcji warunkowej
---	     end if; 						-- zakonczenie instukcji warunkowej 
-
---	   end if;						-- zakonczenie instukcji warunkowej
-
---         when PARZYSTOSC =>					-- obsluga stanu PARZYSTOSC
---	   if (l_czasu /= T) then				-- badanie odliczania okresu T
---	     l_czasu <= l_czasu + 1;				-- zwiekszenie o 1 stanu licznika czasu
---	   else							-- zakonczenie odliczania czasu T
---             l_czasu <= 0;					-- wyzerowanie licznika czasu bodu
---	     stan    <= STOP;					-- przejscie do stanu STOP
---	     if ((wejscie(1) = XOR_REDUCE(bufor)) = N_SLOWO) then -- badanie nieprawidlowej parzystosci bitow
---               problem <= '1';					-- ustawienie rejestru bledu odbioru
---	     end if; 						-- zakonczenie instukcji warunkowej 
---	   end if;						-- zakonczenie instukcji warunkowej
-
---         when STOP =>						-- obsluga stanu STOP
---	   if (l_czasu /= T) then				-- badanie odliczania okresu T
---	     l_czasu <= l_czasu + 1;				-- zwiekszenie o 1 stanu licznika czasu
---	   else							-- zakonczenie odliczania czasu T
---             l_czasu <= 0;					-- wyzerowanie licznika czasu bodu
-
---	     if (l_bitow /= B_STOPOW-1) then			-- badanie odliczania bitow stopu
---	       l_bitow <= l_bitow + 1;				-- zwiekszenie o 1 liczby bitow stopu
---	       if (wejscie(1) /= '0') then			-- badanie nieprawidlowego stanu bitu STOP
---                 problem <= '1';				-- ustawienie rejestru bledu odbioru
---	       end if; 						-- zakonczenie instukcji warunkowej 
---	     else						-- zakonczenie odliczania bitow stopu
---	       if (problem = '0' and wejscie(1) = '0') then	-- badanie prawidlowego odbioru szeregowego
---                 SLOWO <= bufor;				-- ustawienie na wyjsciu SLOWO odebranego slowa 
---                 if (N_SLOWO = TRUE) then			-- badanie warunku zanegowania odebranego slowa
---                   SLOWO <= not(bufor);				-- ustawienie na wyjsciu SLOWO odebranego slowa 
---                 end if;					-- zakonczenie instukcji warunkowej  
---                 GOTOWE <= '1';					-- ustawienie na wyjsciu flagi potwierdzenia
---	       else						-- wykryto nieprawidlowy odbioru szeregowy
---                 SLOWO <= (others => '0');			-- wyzerowanie wyjscia danych
---                 BLAD <= '1';					-- ustawienie na wyjsciu flagi bledu odbioru
---	       end if;						-- zakonczenie instukcji warunkowej
---	       stan <= CZEKANIE;				-- przejscie do stanu CZEKANIE
---	     end if;						-- zakonczenie instukcji warunkowej 
-
---	   end if;						-- zakonczenie instukcji warunkowej
-
---       end case;						-- zakonczenie instukcji warunkowego wyboru
-
---     end if;							-- zakonczenie instukcji warunkowej porcesu
-
-   end process;							-- zakonczenie ciala procesu
+   end process;						
    
 end behavioural;

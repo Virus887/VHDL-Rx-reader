7ca2227ad8246f8163bda2b98f68d6e344b3f4ca
Damian Bis <dambis994132@gmail.com>
2021-04-29 15:07:17 +0200

    Naprawa

--- PlikiDoDodania/signal_simp.vhd
+++ PlikiDoDodania/signal_simp.vhd
@@ -24,117 +24,156 @@ entity SERIAL_RX is
 end SERIAL_RX;
 
 architecture behavioural of SERIAL_RX is
+	
+  signal   R1,R2    :std_logic;				
+  
+  shared variable flag_czekanie    :std_logic := '1';
+  shared variable flag_start       :std_logic := '0';
+  shared variable flag_dana        :std_logic := '0';
+  shared variable flag_parzystosc  :std_logic := '0';
+  shared variable flag_stop        :std_logic := '0';
+  
 
-  signal   input	:std_logic_vector(0 to 1);		-- podwojny rejestr sygnalu RX
-  signal   R1,R2    :std_logic_vector(0 to 1);
-  type     ETAP		is (CZEKANIE, START, DANA, PARZYSTOSC, STOP); -- lista etapow pracy odbiornika
-  signal   state		:ETAP;					-- rejestr maszyny stanow odbiornika
-
-  constant T		:positive := F_ZEGARA/L_BODOW-1;	-- czas jednego bodu - liczba taktów zegara
-  signal   time_counter  	:natural range 0 to T;			-- licznik czasu jednego bodu
-  signal   buf_counter  	:natural range 0 to B_SLOWA-1;		-- licznik odebranych bitow danych lub stopu
+  constant T                        :positive := F_ZEGARA/L_BODOW;	    -- czas jednego bodu - liczba taktów zegara
+  signal   time_counter  	        :natural range 0 to T;			    -- licznik czasu jednego bodu
+  signal   buf_counter  	        :natural range 0 to B_SLOWA-1;		-- licznik odebranych bitow danych lub stopu
+  signal   even_counter  	        :natural range 0 to B_SLOWA-1;		-- licznik odebranych bitow danych lub stopu
+  signal   stop_counter  	        :natural range 0 to B_STOPOW-1;		-- licznik odebranych bitow danych lub stopu
+  
+  
   
   signal   buf	    :std_logic_vector(SLOWO'range);		-- rejestr kolejno odebranych bitow danych
-  signal   error	:std_logic;				-- rejestr (flaga) wykrytego bledu odbioru
+  signal   error	:std_logic;				            -- rejestr (flaga) wykrytego bledu odbioru
+
 
 begin
 
    process (R, C) is						-- proces odbiornika
    begin							-- cialo procesu odbiornika
 
-   R1 <= input;
-   R2 <= R1;
+
    
    if (R = '1') then 
-    input <= (others => '0');
-    R1 <= (others => '0');
-    R2 <= (others => '0');
-    state <= CZEKANIE;
-    time_counter <= 0;
-    buf_counter <= 0;
-    buf <= (others => '0');
-    error <= '0';
+        R1 <=  '0';
+        R2 <= '0';
+        flag_czekanie := '1';
+        flag_start := '0';
+        flag_dana := '0';
+        flag_parzystosc := '0';
+        flag_stop := '0';
+        time_counter <= 0;
+        buf_counter <= 0;
+        even_counter <= 0;
+        stop_counter <= 0;
+        buf <= (others => '0');
+        error <= '0';
     
-    
-   elsif rising_edge(C) then 
-   
-    case state is
-        when CZEKANIE =>					
-           time_counter <= 0;					
-           buf_counter <= 0;					
-           buf   <= (others => '0');				
-           error <= '0';					
-           if (input(1)='0' and input(0)='1') then	
-             state   <= START;					
-           end if;					
-        when START =>
+   elsif rising_edge(C) then  
+      R1 <= RX;
+      R2 <= R1;       
+       if (flag_czekanie = '1') then	--sytuacja czekanie
+           if (R1 = '1' and R2 = '0') then -- narastaj¹ce zbocze
+                flag_czekanie := '0';
+                flag_start := '1';
+                flag_dana := '0';
+                flag_parzystosc := '0';
+                flag_stop := '0';
+                time_counter <= 0;					
+                buf_counter <= 0;					
+                buf <= (others => '0');												
+           end if;	
+           				
+       elsif (flag_start = '1') then
             if (time_counter /= T/2) then
                 time_counter <= time_counter +1;
             else
                 time_counter <= 0;
-                state <= DANA;
-                if(input(1) = '0') then
+                flag_czekanie := '0';
+                flag_start := '0';
+                flag_dana := '1';
+                flag_parzystosc := '0';
+                flag_stop := '0';
+                if(RX = '0') then
                     error <= '1';
                 end if;
             end  if;
     
-        when DANA =>
+       elsif (flag_dana = '1') then
             if(time_counter /= T) then
                 time_counter <= time_counter + 1;
             else
-                buf(buf'left) <= input(1);
-                buf(buf'left -1 downto 0) <= buf(buf'left downto 0);
-                time_counter <= 0;
-                
+                buf(buf_counter) <= RX;
+                time_counter <= 0;             
                 if(buf_counter /= B_SLOWA-1) then
                     buf_counter <= buf_counter + 1;
+                    if (RX = '1') then 
+                        even_counter <= even_counter +1;
+                    end if;
                 else
                     buf_counter <= 0;
                     if(B_PARZYSTOSCI = 1) then
-                        state <= PARZYSTOSC;
+                        flag_czekanie := '0';
+                        flag_start := '0';
+                        flag_dana := '0';
+                        flag_parzystosc := '1';
+                        flag_stop := '0';
                     else
-                        state <= STOP;
+                        flag_czekanie := '0';
+                        flag_start := '0';
+                        flag_dana := '0';
+                        flag_parzystosc := '0';
+                        flag_stop := '1';
                     end if;
-                end if;
-            end if;
+                 end if;
+              end if;
     
-        when PARZYSTOSC =>
+        elsif (flag_parzystosc = '1') then
            if (time_counter /= T) then				
-             time_counter <= time_counter + 1;		
+                time_counter <= time_counter + 1;		
            else							
-                 time_counter <= 0;					
-             state <= STOP;				
-             if ((input(1) = XOR_REDUCE(buf)) = N_SLOWO) then
+                time_counter <= 0;					
+                flag_czekanie := '0';
+                flag_start := '0';
+                flag_dana := '0';
+                flag_parzystosc := '0';
+                flag_stop := '1';				
+                if (even_counter mod 2 /= 0) then
                    error <= '1';					
-             end if; 					
+                end if; 					
            end if;			
 	   
-        when STOP =>						
+        elsif (flag_stop = '1') then					
             if (time_counter /= T) then				
-             time_counter <= time_counter + 1;			
+                 time_counter <= time_counter + 1;			
             else							
-                 time_counter <= 0;					
-            
-             if (buf_counter /= B_STOPOW-1) then			
-               buf_counter <= buf_counter + 1;				
-               if (input(1) /= '0') then		
-                     error <= '1';			
-               end if; 					
-             else					
-               if (error = '0' and input(1) = '0') then	
-                     SLOWO <= buf;				
-                     if (N_SLOWO = TRUE) then			
-                       SLOWO <= not(buf);				
-                     end if;				
-                     GOTOWE <= '1';					
-               else					
+                 time_counter <= 0;	
+                 stop_counter <= stop_counter + 1;				      
+                 if (stop_counter /= B_STOPOW-1) then	
+                    stop_counter <= stop_counter+1;						
+                    if (RX /= '0') then		
+                       error <= '1';			
+                    end if; 					
+                 else					
+                   if (error = '0' and RX = '0') then	
+                         				
+                         if (N_SLOWO = TRUE) then			
+                           SLOWO <= not(buf);
+                         else 
+                           SLOWO <= buf;			
+                         end if;				
+                         GOTOWE <= '1';					
+                   else					
                      SLOWO <= (others => '0');		
                      BLAD <= '1';			
                end if;					
-               state <= CZEKANIE;			
+                flag_czekanie := '1';
+                flag_start := '0';
+                flag_dana := '0';
+                flag_parzystosc := '0';
+                flag_stop := '0';			
              end if;					
-            end if;				
-    end case;
+          end if;				
+       end if;				
    end if;
 
    end process;						
